10edb787676150a244d8bab75c4dc74e016b7bc3
Leandro Argañaraz <49342480+leandromatiasarganaraz@users.noreply.github.com>
2021-01-29 14:38:27 -0300

    Update Examen_Meli.php

--- Examen_Meli.php
+++ Examen_Meli.php
@@ -2,7 +2,7 @@
 <html lang="es">
 <head>
 	<meta charset="UTF-8">
-	<title>Modficar Usuarios</title>
+	<title>Respuesta Meli</title>
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<!-- Latest compiled and minified CSS -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
